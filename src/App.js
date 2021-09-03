3ccc5a3973a0a6ed035fd153fb1ada517b62f616
Alan Rente <alan.rente@iksolution.com.br>
2021-09-03 02:52:32 -0300

    criando formulario de cadastro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { FormCadastro } from './components/FormCadastro';
+import './static/cadastroDevedores.css'
 
 function App() {
   return (
     <div className="App">
       <h1>App Contas</h1>
+      <FormCadastro/>
     </div>
   );
 }
