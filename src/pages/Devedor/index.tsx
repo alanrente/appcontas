8f14e0ea2d4fea95c70c1b8c8adb25ae5a2c71b6
Alan Rente <alan.rente@iksolution.com.br>
2022-02-11 05:14:00 -0300

    update

--- src/pages/Devedor/index.tsx
+++ src/pages/Devedor/index.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+
 import { Typography } from "@material-ui/core";
 
+import { getDevedores } from "../../services/devedores/index";
+
 export function Devedor() {
+  async function handleGetDevedores() {
+    const data = await getDevedores();
+
+    return data;
+  }
+
+  useEffect(() => {
+    handleGetDevedores();
+  }, []);
+
   return (
     <>
       <Typography variant="h4">Devedor</Typography>
