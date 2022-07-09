0551d51db28dc683491744d645e456f2270bba93
Alan Rente <alan.rente@iksolution.com.br>
2022-07-09 02:32:14 -0300

    change layout

--- src/services/devedores/index.tsx
+++ src/services/devedores/index.tsx
@@ -1,31 +1,7 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 
-/* eslint-disable no-console */
 export async function getDevedores() {
-  const options: AxiosRequestConfig = {
-    method: "GET",
-    url: `${process.env.REACT_APP_API_URL || "http://localhost:3000/api"}/devedores`,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  const { data } = await axios.request(options);
-
-  console.log(data);
-}
-
-export async function upsertDevedor(devedor: string) {
-  const options: AxiosRequestConfig = {
-    method: "POST",
-    url: `${process.env.REACT_APP_API_URL || "http://localhost:3000/api"}/devedores`,
-    data: { nome: devedor },
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  const { data } = await axios.request(options);
+  const { data } = await axios.get("/pessoas");
 
   console.log(data);
 }
